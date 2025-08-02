ea04a0217bcc75fda6be3bd92bef2d50d0ed8844
Niko2281337 <Okhlopkovnikita@mail.ru>
2025-08-02 21:37:34 +0300

    add apps

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'apps.accounts',
+    'apps.profiles',
+    'apps.sellers',
+    'apps.shop',
 ]
 
 MIDDLEWARE = [
