5ab898afdd4bdc010d0c3828f39a624a55343036
Niko2281337 <Okhlopkovnikita@mail.ru>
2025-08-02 23:02:43 +0300

    add User and ShippingAddress models and custom manager for User model

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class ShippingAddress(BaseModel):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="shipping_addresses"
+    )
+    full_name = models.CharField(max_length=1000)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.full_name}'s shipping details"
--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = "accounts.User"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
