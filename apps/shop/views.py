aa24232132fc7c589a61c0de072b532c0270d3c5
Niko2281337 <Okhlopkovnikita@mail.ru>
2025-08-09 15:02:45 +0300

    add endpoints for cart

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -2,7 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.shop.models import Category, Product
-from apps.shop.serializers import CategorySerializer, ProductSerializer
+from apps.shop.serializers import CategorySerializer, ProductSerializer, ToggleCartItemSerializer, OrderItemSerializer, CheckoutSerializer, OrderSerializer
+
+from apps.profiles.models import OrderItem, ShippingAddress, Order
 
 from apps.sellers.models import Seller
 
@@ -74,3 +76,83 @@ class ProductView(APIView):
             return Response(data={"message": "Product does not exist!"}, status=404)
         serializer = self.serializer_class(product)
         return Response(data=serializer.data, status=200)
+    
+
+class CartView(APIView):
+    serializer_class = OrderItemSerializer
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        orderitems = OrderItem.objects.filter(user=user, order=None).select_related(
+            "product", "product__seller", "product__seller__user")
+        serializer = self.serializer_class(orderitems, many=True)
+        return Response(data=serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        serializer = ToggleCartItemSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        quantity = data["quantity"]
+
+        product = Product.objects.select_related("seller", "seller__user").get_or_none(slug=data["slug"])
+        if not product:
+            return Response({"message": "No Product with that slug"}, status=404)
+        orderitem, created = OrderItem.objects.update_or_create(
+            user=user,
+            order=None,
+            product=product,
+            defaults={"quantity": quantity},
+        )
+        resp_message_substring = "Updated In"
+        status_code = 200
+        if created:
+            status_code = 201
+            resp_message_substring = "Added To"
+        if orderitem.quantity == 0:
+            resp_message_substring = "Removed From"
+            orderitem.delete()
+            data = None
+        if resp_message_substring != "Removed From":
+            serializer = self.serializer_class(orderitem)
+            data = serializer.data
+        return Response(data={"message": f"Item {resp_message_substring} Cart", "item": data}, status=status_code)
+    
+
+class CheckoutView(APIView):
+    serializer_class = CheckoutSerializer
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        orderitems = OrderItem.objects.filter(user=user, order=None)
+        if not orderitems.exists():
+            return Response({"message": "No Items in Cart"}, status=404)
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        shipping_id = data.get("shipping_id")
+        if shipping_id:
+            shipping = ShippingAddress.objects.get_or_none(id=shipping_id)
+            if not shipping:
+                return Response({"message": "No shipping address with that ID"}, status=404)
+
+        fields_to_update = [
+            "full_name",
+            "email",
+            "phone",
+            "address",
+            "city",
+            "country",
+            "zipcode",
+        ]
+        data = {}
+        for field in fields_to_update:
+            value = getattr(shipping, field)
+            data[field] = value
+
+        order = Order.objects.create(user=user, **data)
+        orderitems.update(order=order)
+
+        serializer = OrderSerializer(order)
+        return Response(data={"message": "Checkout Successful", "item": serializer.data}, status=200)
